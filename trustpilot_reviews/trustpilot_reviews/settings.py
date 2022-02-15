d6c9392d392e6a9d0cfe513231e8cd09a411a6c6
JoGorska <83362036+JoGorska@users.noreply.github.com>
2022-02-15 17:04:45 +0000

    Changed settings for user_agent, but not worked to get through to revew page

--- trustpilot_reviews/trustpilot_reviews/settings.py
+++ trustpilot_reviews/trustpilot_reviews/settings.py
@@ -14,10 +14,10 @@ NEWSPIDER_MODULE = 'trustpilot_reviews.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'trustpilot_reviews (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
