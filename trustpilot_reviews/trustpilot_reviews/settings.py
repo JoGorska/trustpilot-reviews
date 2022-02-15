f396e7d8dac44f9562c11a75047f60fc659dae8b
JoGorska <83362036+JoGorska@users.noreply.github.com>
2022-02-15 16:50:29 +0000

    Fix naming for Item for trustpilot spider

--- trustpilot_reviews/trustpilot_reviews/settings.py
+++ trustpilot_reviews/trustpilot_reviews/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'trustpilot_reviews.spiders'
 #USER_AGENT = 'trustpilot_reviews (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
