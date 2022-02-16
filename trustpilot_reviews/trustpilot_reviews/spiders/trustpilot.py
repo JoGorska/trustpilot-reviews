1988dce9a3bf1888bfb33672b53fbf60e5878a8e
JoGorska <83362036+JoGorska@users.noreply.github.com>
2022-02-16 15:02:37 +0000

    Changed naming convention from review to item in attempt to fix database not creating

--- trustpilot_reviews/trustpilot_reviews/spiders/trustpilot.py
+++ trustpilot_reviews/trustpilot_reviews/spiders/trustpilot.py
@@ -15,24 +15,24 @@ class TrustpReviewSpider(scrapy.Spider):
     # list obtained by running match_y_tp.py inside stiemaps directory
 
     def parse(self, response):
-        review = TrustpilotReviewsItem()
+        item = TrustpilotReviewsItem()
         company_name = response.xpath('//h1/span/text()').extract_first()
         summary = response.xpath('//p/text()').extract_first()
-        review["company_name"] = company_name
-        review["summary"] = summary
+        item["company_name"] = company_name
+        item["summary"] = summary
         article_container = response.xpath('//article').extract()
         print(len(article_container))
 
         all_headers_with_commas = response.xpath('//h2/a/text()').extract()
         all_headers = []
-        for item in all_headers_with_commas:
-            new_item = item.replace(",", "")
-            all_headers.append(new_item)
+        for header_item in all_headers_with_commas:
+            new_header_item = header_item.replace(",", "")
+            all_headers.append(new_header_item)
         all_content_with_commas = response.xpath('//div[@class="styles_reviewContent__0Q2Tg"]/p/text()').extract()
         all_content = []
-        for item in all_content_with_commas:
-            new_item = item.replace(",", "")
-            all_content.append(new_item)
+        for content_item in all_content_with_commas:
+            new_content_item = content_item.replace(",", "")
+            all_content.append(new_content_item)
         if len(all_headers) >= 3:
             one_header = all_headers[1]
             one_content = all_content[0]
@@ -42,12 +42,12 @@ class TrustpReviewSpider(scrapy.Spider):
 
             three_header = all_headers[3]
             three_content = all_content[2]
-            review["one_header"] = one_header
-            review["one_content"] = one_content
-            review["two_header"] = two_header
-            review["two_content"] = two_content
-            review["three_header"] = three_header
-            review["three_content"] = three_content
+            item["one_header"] = one_header
+            item["one_content"] = one_content
+            item["two_header"] = two_header
+            item["two_content"] = two_content
+            item["three_header"] = three_header
+            item["three_content"] = three_content
         elif len(all_headers) == 2:
             one_header = all_headers[1]
             one_content = all_content[0]
@@ -55,17 +55,17 @@ class TrustpReviewSpider(scrapy.Spider):
             two_header = all_headers[2]
             two_content = all_content[1]
 
-            review["one_header"] = one_header
-            review["one_content"] = one_content
-            review["two_header"] = two_header
-            review["two_content"] = two_content
+            item["one_header"] = one_header
+            item["one_content"] = one_content
+            item["two_header"] = two_header
+            item["two_content"] = two_content
         elif len(all_headers) == 0:
             one_header = all_headers[1]
             one_content = all_content[0]
 
-            review["one_header"] = one_header
-            review["one_content"] = one_content
-        yield review
+            item["one_header"] = one_header
+            item["one_content"] = one_content
+        yield item
         # test_url = 'https://uk.trustpilot.com/review/garolla.co.uk'
         truspilot_domains_list = ['https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/t.co', 'https://uk.trustpilot.com/review/ring.com', 'https://uk.trustpilot.com/review/inoautocentres.co.uk', 'https://uk.trustpilot.com/review/inoautocentres.co.uk', 'https://uk.trustpilot.com/review/garolla.co.uk', 'https://uk.trustpilot.com/review/etyres.co.uk', 'https://uk.trustpilot.com/review/etyres.co.uk', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.facebook.com', 'https://uk.trustpilot.com/review/www.ratedpeople.com', 'https://uk.trustpilot.com/review/www.national.co.uk', 'https://uk.trustpilot.com/review/www.national.co.uk', 'https://uk.trustpilot.com/review/www.national.co.uk', 'https://uk.trustpilot.com/review/ring.co.uk', 'https://uk.trustpilot.com/review/ring.co.uk', 'https://uk.trustpilot.com/review/sheds.co.uk', 'https://uk.trustpilot.com/review/ge.com', 'https://uk.trustpilot.com/review/ge.com', 'https://uk.trustpilot.com/review/thegaragedoorcentre.co.uk', 'https://uk.trustpilot.com/review/www.mrclutch.com', 'https://uk.trustpilot.com/review/eon.com', 'https://uk.trustpilot.com/review/www.centralautopoint.com']
 
