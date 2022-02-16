ac433f7952da17510fb9322276954d08e57db3a1
JoGorska <83362036+JoGorska@users.noreply.github.com>
2022-02-16 11:20:06 +0000

    Tidied up trustpilot spider

--- trustpilot_reviews/trustpilot_reviews/spiders/trustpilot.py
+++ trustpilot_reviews/trustpilot_reviews/spiders/trustpilot.py
@@ -19,18 +19,9 @@ class TrustpReviewSpider(scrapy.Spider):
         review["summary"] = summary
         article_container = response.xpath('//article').extract()
         print(len(article_container))
-        containers_list = []
-        if len(article_container) >= 3:
-            containers_list = [article_container[0], article_container[1], article_container[2]]
-        elif len(article_container) == 2:
-            containers_list = [article_container[0], article_container[1]]
-        elif len(article_container) == 0:
-            containers_list = [article_container[0]]
-        print(f'LIST OF CONTAINERS LENGTH {len(containers_list)}')
 
         all_headers = response.xpath('//h2/a/text()').extract()
         all_content = response.xpath('//div[@class="styles_reviewContent__0Q2Tg"]/p/text()').extract()
-        print(f'ALL CONTENT LIST {all_content}')
         if len(all_headers) >= 3:
             one_header = all_headers[0]
             one_content = all_content[0]
@@ -63,12 +54,5 @@ class TrustpReviewSpider(scrapy.Spider):
 
             review["one_header"] = one_header
             review["one_content"] = one_content
-        
-        # print(f'ONE HEADER FROM EACH ARTICLE {one_header}')
-
-        # print(first_three)
-        # one_header = response.xpath('//')
-        
-        # print(f'WHAT DOES MY ITEM RETURN{review}')
         
         yield review
